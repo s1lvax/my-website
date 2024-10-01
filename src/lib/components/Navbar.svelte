2febe66f4ca7f307ad77b8bb4860169371920a14
s1lvax <s1lvax@proton.me>
2024-10-01 18:28:56 +0200

    added logo

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,13 @@
-<header class="z-50 flex w-full flex-wrap py-7 md:flex-nowrap md:justify-start">
+<header class="z-50 flex w-full flex-wrap md:flex-nowrap md:justify-start">
 	<nav
 		class="relative mx-auto flex w-full max-w-7xl basis-full flex-wrap items-center px-4 md:grid md:grid-cols-12 md:px-6"
 	>
-		<div class="md:col-span-3">
-			<a href="/">
-				<h1 class="text-2xl font-bold text-white">Cesario Silva</h1>
-			</a>
-		</div>
+		<a href="/" class="h-32 w-32 pt-2 md:mb-6 md:pt-4 xl:pt-8">
+			<img
+				src="/logo.svg"
+				alt=""
+				class="mb-0 mt-4 h-32 w-32 transition-transform hover:scale-105"
+			/>
+		</a>
 	</nav>
 </header>

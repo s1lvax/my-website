b39d1140a8cd18651e10e43177b1eb902dea53d4
s1lva <s1lvax@proton.me>
2024-10-01 13:15:48 +0200

    Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<div class="mx-auto flex size-full max-w-[50rem] flex-col">
+	<main id="content">
+		<div class="px-4 py-10 text-center sm:px-6 lg:px-8">
+			<h1 class="block text-7xl font-bold text-gray-800 sm:text-9xl dark:text-white">Error</h1>
+			<p class="mt-3 text-gray-600 dark:text-neutral-400">Oops, something went wrong.</p>
+			<p class="text-gray-600 dark:text-neutral-400">Sorry, we couldn't find your page.</p>
+			<div class="mt-5 flex flex-col items-center justify-center gap-2 sm:flex-row sm:gap-3">
+				<a
+					class="inline-flex transform items-center justify-center gap-x-2 rounded-2xl border border-transparent bg-[#FCC237] px-4 py-3 text-sm font-medium text-black transition-transform hover:scale-105 focus:outline-none active:scale-95 disabled:pointer-events-none disabled:opacity-50"
+					href="/"
+				>
+					<svg
+						class="size-4 shrink-0"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"><path d="m15 18-6-6 6-6" /></svg
+					>
+					Back to the main page
+				</a>
+			</div>
+		</div>
+	</main>
+</div>

9f56809108bf0d55200c3bbb478caad079e789b0
s1lvax <s1lvax@proton.me>
2024-10-05 17:30:12 +0200

    Contact page mobile layout

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import ContactForm from '$lib/components/ContactForm.svelte';
 	import ContactIcons from '$lib/components/ContactIcons.svelte';
-	import SuperDebug from 'sveltekit-superforms';
 
 	export let data;
 </script>
 
-<div class="mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14">
+<main class="mx-auto flex h-screen max-w-[85rem] flex-col px-4 sm:px-6 lg:px-8">
 	<div class="mx-auto max-w-xl">
 		<div class="text-center">
 			<h1 class="text-3xl font-bold text-white sm:text-4xl">Contact me</h1>
@@ -16,4 +15,4 @@
 
 	<ContactForm data={data.form} />
 	<ContactIcons />
-</div>
+</main>
